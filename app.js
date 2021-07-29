2530c376c9da7781fabce24f9227e8cea42f98b5
Arpita1899 <arpitaverma8885@gmail.com>
2021-07-29 12:45:41 +0530

    hypotenuse

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var input1=document.getElementById("input-text1");
 var input2=document.getElementById("input-text2");
 var input3=document.getElementById("input-text3");
-var output=document.getElementById("output");
 
-var submitBtn=document.getElementById("submit-btn");
-function clickEventHandler(){
+var output=document.getElementById("output");
+var outputHypo=document.getElementById("output-hypo");
+
+var inputA=document.getElementById("input-a");
+var inputB=document.getElementById("input-b");
+
+var submitangles=document.getElementById("submit-angles");
+var submitHypotenuse=document.getElementById("submit-hypotenuse");
+
+function clickAngleHandler(){
     if(Number(input1.value)+Number(input2.value)+Number(input3.value)===180){
         output.innerText="Hurray! You can make a triangle with these angles."
     }
@@ -14,5 +21,17 @@ function clickEventHandler(){
     }
 
 }
+submitHypotenuse.addEventListener('click',clickHypotenuseHandler);
 
-submitBtn.addEventListener('click',clickEventHandler);
+function clickHypotenuseHandler(){
+    var a=inputA.value;
+    var prod=a*a;
+    var b=inputB.value;
+    var prod2=b*b;
+    var c=Math.sqrt(prod2+prod);
+    outputHypo.innerText=c;
+   
+
+}
+
+submitangles.addEventListener('click',clickAngleHandler);

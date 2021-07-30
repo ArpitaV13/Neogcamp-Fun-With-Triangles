0c0f6e3feaf057c48209b5e1c1eddb46ee2865dd
Arpita1899 <arpitaverma8885@gmail.com>
2021-07-30 23:58:47 +0530

    js file

---
+++ area.js
@@ -0,0 +1,7 @@
+if((document.getElementById('area-1').checked)==true){
+    console.log("rahed");
+    document.getElementById('ouput-area1').style.display="block";
+}
+else{
+    Area1.style.display="none";
+}

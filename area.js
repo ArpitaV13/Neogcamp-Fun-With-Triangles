fc836ac0a78dd0e413bfc8ded640b2e5e4fd066d
Arpita1899 <arpitaverma8885@gmail.com>
2021-07-31 15:30:28 +0530

    area js

--- area.js
+++ area.js
@@ -1,7 +1,33 @@
-if((document.getElementById('area-1').checked)==true){
-    console.log("rahed");
-    document.getElementById('ouput-area1').style.display="block";
-}
-else{
-    Area1.style.display="none";
+// var btn1=document.getElementById('btn-1');
+// btn1.addEventListener('click',areaHandler);
+
+var radioBtns=document.querySelectorAll("areaCal");
+
+function areaHandler(x){
+  if(x==1){
+    document.getElementById("output-area1").style.display="block";
+    document.getElementById("output-area2").style.display="none";
+    document.getElementById("output-area3").style.display="none";
+    document.getElementById("output-area4").style.display="none";
+  }
+  if(x==2){
+    document.getElementById("output-area2").style.display="block";
+    document.getElementById("output-area1").style.display="none";
+    document.getElementById("output-area3").style.display="none";
+    document.getElementById("output-area4").style.display="none";
+  }
+  if(x==3){
+    document.getElementById("output-area3").style.display="block";
+    document.getElementById("output-area1").style.display="none";
+    document.getElementById("output-area2").style.display="none";
+    document.getElementById("output-area4").style.display="none";
+  }
+  if(x==4){
+    document.getElementById("output-area4").style.display="block";
+    document.getElementById("output-area1").style.display="none";
+    document.getElementById("output-area2").style.display="none";
+    document.getElementById("output-area3").style.display="none";
+  }
+  
+   
 }
